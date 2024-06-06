3b9fd373b93d1fd33169cad58082da345cbdaef6
Kollegvertretung der Lernenden am Treptow-Kolleg Berlin <kv@treptow-kolleg.de>
2024-06-06 13:44:58 +0200

    add colors

--- src/Controller/ElectionController.php
+++ src/Controller/ElectionController.php
@@ -75,6 +75,16 @@ class ElectionController extends AbstractController
             '#00b294',
             '#009e49',
             '#bad80a',
+            '#fff100',
+            '#ff8c00',
+            '#e81123',
+            '#ec008c',
+            '#68217a',
+            '#00188f',
+            '#00bcf2',
+            '#00b294',
+            '#009e49',
+            '#bad80a',
         ];
 
         return $this->render('election/show.html.twig', [
