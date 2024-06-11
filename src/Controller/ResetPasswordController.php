e739ec577ce18508b636e930d2f7bd0247911122
Kollegvertretung der Lernenden am Treptow-Kolleg Berlin <kv@treptow-kolleg.de>
2024-06-11 19:18:47 +0200

    update mail

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -121,7 +121,7 @@ class ResetPasswordController extends AbstractController
 
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
-
+            $this->addFlash('success','Passwort wurde erfolgreich zurückgesetzt.');
             return $this->redirectToRoute('app_login');
         }
 
