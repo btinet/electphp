5077c54a9c2fa7d6b4be969c4acec3ffc4e56c00
Kollegvertretung der Lernenden am Treptow-Kolleg Berlin <kv@treptow-kolleg.de>
2024-06-11 19:14:01 +0200

    update mail

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -25,11 +25,11 @@ class ChangePasswordFormType extends AbstractType
                 'first_options' => [
                     'constraints' => [
                         new NotBlank([
-                            'message' => 'Please enter a password',
+                            'message' => 'Bitte ein neues Passwort angeben',
                         ]),
                         new Length([
                             'min' => 6,
-                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            'minMessage' => 'Ihr Passwort sollte mindestens {{ limit }} Zeichen enthalten.',
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
                         ]),
@@ -39,7 +39,7 @@ class ChangePasswordFormType extends AbstractType
                 'second_options' => [
                     'label' => 'Repeat Password',
                 ],
-                'invalid_message' => 'The password fields must match.',
+                'invalid_message' => 'Die Passwörter stimmen nicht überein.',
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
